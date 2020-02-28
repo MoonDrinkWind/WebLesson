23d2b9388e4fd065e55a6d790d1a39e1287d259a
MoonDrinkWind <tanchengzhang520@163.com>
2020-02-28 08:57:01 +0800

    a

--- getLessons.py
+++ getLessons.py
@@ -6,21 +6,21 @@ from lxml import etree
 threadLock = threading.Lock()
 lessonFile = open("lesson.txt", "w+", encoding="utf-8")
 keyWord = input("请输入关键词:")
+url = input("请输入目标:")
 
 def getLesson(start, end):
     for i in range(start, end):
-        url = "http://h5.nty.tv189.com/hv/C424"+ str(i)  +".html"
-        r = requests.get(url, headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}).text
+        r = requests.get(url.format(i), headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}).text
         s = etree.HTML(r)
         titles = s.xpath("/html/head/title/text()")
         print(threading.current_thread().name)
-        print(url)
+        print(url.format(i))
         print(titles)
         for title in titles:
             if keyWord in title:
                 threadLock.acquire()
                 lessonFile.write(title + "\n")
-                lessonFile.write(url + "\n")
+                lessonFile.write(url.format(i) + "\n")
                 threadLock.release()
     print(threading.current_thread().name + "爬取结束")   
 
